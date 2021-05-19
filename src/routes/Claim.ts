9a99a4b87c26d84cc53e975a9e0bc24abdc67e3b
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2021-05-19 13:57:55 -0400

    set isIssued when issuing

--- src/routes/Claim.ts
+++ src/routes/Claim.ts
@@ -151,6 +151,7 @@ export class ClaimRouter {
         const template = parse(award.issuance.credential.template);
         const populated = template(templateVals);
         const { sign } = createIssuer([award.issuance.credential.group.didDoc]);
+        award.isIssued = true;
         award.issuedAt = new Date();
         award.save();
         sign(populated, {
