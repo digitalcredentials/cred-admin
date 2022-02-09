1f055a82319d532722f172db3182a0ac02a7a25b
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2022-02-09 12:39:38 -0500

    log sequelize queries at debug level

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
+import logger from "@shared/Logger";
 
 if (!process.env.CA_DB_CONNECTION_URL) {
   throw new Error("CA_DB_CONNECTION_URL is not defined in evironment!");
 }
 
-const sequelize = new Sequelize(process.env.CA_DB_CONNECTION_URL || "");
+const sequelize = new Sequelize(process.env.CA_DB_CONNECTION_URL || "", {
+  logging: (sql) => logger.debug(sql),
+});
 
 sequelize.addModels([`${__dirname}/models`]);
 
